9e0368ed01d522b3d6f5b2a96d53defe51a5d5f7
tannerwheeler <tannerleonwheeler@gmail.com>
2024-07-31 17:09:20 -0600

    moved roms folder.

--- D5700ComputerEmulator/src/main/kotlin/ROM.kt
+++ D5700ComputerEmulator/src/main/kotlin/ROM.kt
@@ -2,4 +2,10 @@ class ROM : Memory() {
     override fun write(position: UInt, value: UByte) {
         TODO("Not yet implemented")
     }
+
+    fun loadROM() {
+        println("Type in the path to the rom file:")
+        val path = readLine()
+        //TODO: Finish loading
+    }
 }
