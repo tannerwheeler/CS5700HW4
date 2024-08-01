10e8605d62ca56286ddb9b509403bb3ecdb71f79
tannerwheeler <tannerleonwheeler@gmail.com>
2024-08-01 10:25:51 -0600

    Implemented ConvertToBase10 and its tests.

--- D5700ComputerEmulator/src/main/kotlin/instructions/ConvertToBase10.kt
+++ D5700ComputerEmulator/src/main/kotlin/instructions/ConvertToBase10.kt
@@ -10,10 +10,20 @@ class ConvertToBase10(
     display: Display
 ) : Instruction(cpu, memory, display) {
     override fun split() {
-        TODO("Not yet implemented")
+        val tempHolder1 = splitByte(byte1)
+        mySplit.add(tempHolder1.first)
+        mySplit.add(tempHolder1.second)
+
+        require(byte2 == 0.toUByte()) { "Second Byte is not 00 in ConvertToBase10 Instruction" }
     }
 
     override fun perform() {
-        TODO("Not yet implemented")
+        require(mySplit[0] == 13u) { "First 4 bits of first byte must be D in ConvertToBase10 Instruction" }
+        var needsConversion : Int = cpu.registers[mySplit[1].toInt()].toInt()
+        memory[cpu.memory.toInt()].write(cpu.address.toInt() + 2, (needsConversion % 10).toByte())
+        needsConversion /= 10
+        memory[cpu.memory.toInt()].write(cpu.address.toInt() + 1, (needsConversion % 10).toByte())
+        needsConversion /= 10
+        memory[cpu.memory.toInt()].write(cpu.address.toInt(), (needsConversion % 10).toByte())
     }
 }
---
+++ D5700ComputerEmulator/src/test/kotlin/instructionsTest/ConvertToBase10Test.kt
@@ -0,0 +1,44 @@
+package instructionsTest
+
+import CPU
+import ROM
+import RAM
+import Display
+import instructions.ConvertToBase10
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class ConvertToBase10Test {
+    val cpu = CPU()
+    val array = arrayOf(RAM(), ROM())
+    val display = Display()
+    val convertToBase10 = ConvertToBase10(cpu, array, display)
+
+    @Test
+    fun testConvertToBase10() {
+        cpu.registers[2] = 123
+        convertToBase10.execute("D2","00")
+        assertEquals(1, array[0].read(cpu.address.toInt()))
+        assertEquals(2, array[0].read(cpu.address.toInt()+1))
+        assertEquals(3, array[0].read(cpu.address.toInt()+2))
+    }
+
+    @Test
+    fun testBadConvertToBase10Test() {
+        val block: () -> Unit = { convertToBase10.execute("70", "00") }
+        assertFailsWith<IllegalArgumentException> { block() }
+    }
+
+    @Test
+    fun testBadConvertToBase10Test2() {
+        val block: () -> Unit = { convertToBase10.execute("D0", "10") }
+        assertFailsWith<IllegalArgumentException> { block() }
+    }
+
+    @Test
+    fun testBadConvertToBase10Test3() {
+        val block: () -> Unit = { convertToBase10.execute("D0", "02") }
+        assertFailsWith<IllegalArgumentException> { block() }
+    }
+}
