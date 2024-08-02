ea94ca41b1d2f2f4bcc704a19198d3e640cc3b40
tannerwheeler <tannerleonwheeler@gmail.com>
2024-08-01 19:43:25 -0600

    Need to read in the Bytes of the .out files. First attempt at solving the problem.

--- D5700ComputerEmulator/src/main/kotlin/Main.kt
+++ D5700ComputerEmulator/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 import instructions.Store
 
 fun main() {
-    //D5700Computer()
+    ROM().loadROM()
 
 }
--- D5700ComputerEmulator/src/main/kotlin/ROM.kt
+++ D5700ComputerEmulator/src/main/kotlin/ROM.kt
@@ -1,19 +1,28 @@
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class ROM : Memory() {
     override fun write(position: Int, value: UByte) {
         //TODO("Not yet implemented")
     }
 
+    @OptIn(ExperimentalStdlibApi::class)
     override fun loadROM() {
         println("Type in the path to the rom file:")
         val path = readln()
         var position = 0
-        File(path).forEachLine {
-            val bytes = it.chunked(2)
-            data[position] = bytes[0].toUByte(16)
-            data[position + 1] = bytes[1].toUByte(16)
-            position += 2
+        val path2 = Paths.get(path)
+        val newData = Files.readAllBytes(path2).toUByteArray()
+        newData.forEach {
+            println(it)
         }
+
+//        File(path).forEachLine {
+//            val bytes = it.chunked(2)
+//            data[position] = bytes[0].toUByte(16)
+//            data[position + 1] = bytes[1].toUByte(16)
+//            position += 2
+//        }
     }
 }
